2556d61bafc87c0cfd2822f5c95645bb1b5ce171
kazem <mohammadkazemfarahbakhsh5@gmail.com>
2026-07-07 04:14:10 +0330

    action:crud

--- app/outlook/professors/[slug]/page.tsx
+++ app/outlook/professors/[slug]/page.tsx
@@ -1,14 +1,37 @@
-import React from 'react';
-import ClientProfessor from './ClientProfessor';
+import React from "react";
+import ClientProfessor from "./ClientProfessor";
+import { api } from "@/utils/api/base";
+import { IProfessor } from "@/entity/professor";
+import { AxiosResponse } from "axios";
+
 export const metadata = {
   title: "استاد شهشانی",
-}
-const Page = () => {
+};
+
+const Page = async ({ params }: { params: Promise<{ slug: string }> }) => {
+  const { slug } = await params;
+
+  let data: IProfessor = {} as IProfessor;
+  try {
+    console.log(api.defaults.baseURL);
+    console.log(`/academic/professor/${slug}`);
+  
+    const result: AxiosResponse<IProfessor> = await api.get(
+      `/academic/professor/${slug}`,
+    );
+
+    console.log(result.data.name);
+
+    data = result.data;
+  } catch (err) {
+    console.log(err);
+  }
+
   return (
     <div>
-      <ClientProfessor/>
+      <ClientProfessor data={data} />
     </div>
   );
-}
+};
 
 export default Page;
